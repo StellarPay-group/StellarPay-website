aebd8c184c34b45f926b7b86f491b40569d75f34
rohiths2 <rohiths2@illinois.edu>
2025-08-24 22:21:31 -0500

    fixed sign-up/sign-in pages

--- app/(public)/signin/page.tsx
+++ app/(public)/signin/page.tsx
@@ -2,13 +2,19 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState } from "react";
+import GetTheApp from "@/components/popup/getTheApp";
+
 
 export default function SignInPage() {
+
+  const [showPopup, setShowPopup] = useState(false);    
+
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="flex flex-row justify-between items-center p-4">
+      <header className="flex flex-row justify-between items-center p-2">
         <Link href="/">
-          <Button variant="ghost" className="text-xl md:text-[24px] mb-1 font-bold text-[#191c1f]">StellarPay</Button>
+          <Button variant="ghost" className="text-xl md:text-[24px] font-bold text-[#191c1f]">StellarPay</Button>
         </Link>
       </header>
 
@@ -16,24 +22,25 @@ export default function SignInPage() {
 
 
 
-      <main className="flex flex-col justify-center items-center p-8">
+      <main className="flex flex-col justify-center items-center mb-[50px]">
 
-
-        <div className="flex flex-row justify-center items-center">
-          <div className="flex flex-col">
-            <Image src="/images/stellarbanking_logo.png" alt="StellarPay" width={100} height={100} className="mx-auto mb-[20px]" />
-            <h1 className="text-4xl font-bold mb-4">Welcome Back</h1>
-            <p className="text-md text-[#191c1f] mb-[20px]">Enter your phone number</p>
+      <Image src="/images/stellarbanking_logo.png" alt="StellarPay" width={100} height={100} className="mx-auto mb-[20px]" />
+        <div className="flex flex-col md:flex-row justify-center items-center md:translate-x-[-24px]">
+          <div className="flex flex-col max-w-lg mr-[50px]">
+            <h1 className="text-4xl font-semibold mb-4">Welcome back</h1>
+            <div className="max-w-md">
+            <p className="text-md text-gray-700 mb-[40px]">Enter the phone number associated with your StellarPay account</p>
+            </div>
             <div className="flex flex-row">
-              <input type="text" placeholder="+1" className="mb-[20px] w-[60px] lg:h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px] mr-[10px]" onChange={(e) => { }} maxLength={4} />
+              <input type="text" placeholder="+1" className="mb-[20px] w-[60px] lg:h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px] mr-[20px]" onChange={(e) => { }} maxLength={4} />
               <input type="text" placeholder="Mobile number" className="w-[340px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px]" onChange={(e) => { }} maxLength={10} />
             </div>
-            <Link href="/api/auth/signin">
-              <button className="bg-[#0065ff] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px]">Continue</button>
-            </Link>
-            <p className="text-center text-md text-[#191c1f] mt-[20px]">OR</p>
-            <div className="flex flex-row justify-center items-center">
-              <Link href="/api/auth/signin">
+            <a onClick={() => setShowPopup(true)}>
+              <button className="bg-[#000000] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px] mt-[5px]">Continue</button>
+            </a>
+            <p className="text-center text-md text-[#191c1f] mt-[20px] mr-[20px]">OR</p>
+            <div className="flex flex-row justify-between items-center border-gray mr-[20px]">
+            <a onClick={() => setShowPopup(true)}>
                 <button className="mt-[20px] w-[200px] h-[60px] rounded-[10px] border 
               border-gray-300 p-[10px] text-[18px] mr-[10px]">
                 <div className="flex flex-row items-center">
@@ -41,38 +48,40 @@ export default function SignInPage() {
                   <p className="text-[16px] ml-[10px]">Sign in with Google</p>
                 </div>
               </button>
-              </Link>
-              <Link href="/api/auth/signin">
+              </a>
+              <a onClick={() => setShowPopup(true)}>
                 <button className="mt-[20px] w-[200px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[18px] mr-[10px]">
                   <div className="flex flex-row items-center">
                     <Image src="/images/AppleLogo.png" alt="Apple" width={20} height={20} />
                     <p className="text-[16px] ml-[10px]">Sign in with Apple</p>
                   </div>
                 </button>
-              </Link>
+              </a>
             </div>
-            <p className="mt-[20px] text-md">Don't have an account? </p>
+            <p className="mt-[20px] text-center mr-[20px] text-md">Don't have an account? </p>
             <Link href="/signup">
-              <button className="mt-[20px] bg-[#0065ff] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px]">Create account</button>
+              <button className="mt-[20px] bg-[#000000] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px]">Create account</button>
             </Link>
           </div>
 
-          <div className="flex flex-col justify-center items-center ml-[100px]">
-            <Image src="/images/qr-code.png" alt="QR Code" width={200} height={200} className="mb-[20px]" />
-            <p className="text-center text-lg font-bold text-[#191c1f]">Log in with QR code</p>
-            <p className="text-center text-md text-[#191c1f] mt-[20px] w-[300px]">Scan this code with your phone camera to log in instantly</p>
+          <div className="hidden md:block flex flex-col justify-center items-center ml-[100px]">
+            <Image src="/images/qr-code.png" alt="QR Code" width={200} height={200} className="mb-[20px] border-gray-700 border-1 rounded-2xl shadow-xl ml-[25px]" />
+            <p className="text-center text-lg font-bold text-[#191c1f] mt-[20px]">Log in with QR code</p>
+            <p className="text-center text-md text-gray-700 mt-[20px] w-[250px]">Scan this code with your phone camera to log in instantly</p>
           </div>
 
         </div>
       </main>
 
 
-      <div className="absolute flex flex-row left-[40px] bottom-[10px]">
-        <p className="mr-[60px] text-sm md:text-md">English (United States)</p>
+      <div className="fixed flex flex-row left-[60px] bottom-[30px]">
+        <p className="mr-[30px] text-sm md:text-md">English (United States)</p>
         <Link href="/privacy-policy" className="block text-[#000000] underline hover:no-underline text-sm md:text-md">
           Privacy policy
         </Link>
       </div>
+
+      {showPopup && <GetTheApp onClose={() => setShowPopup(false)} onSubmit={() => {}} />}
 
     </div>
   );
--- app/(public)/signup/page.tsx
+++ app/(public)/signup/page.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState } from "react";
+import GetTheApp from "@/components/popup/getTheApp";
 
 const validateCountryCode = (countryCode: string) => {
           return countryCode.match(/^\+[0-9](?:[0-9]{0,2})$/);
@@ -14,75 +16,81 @@ const validatePhoneNumber = (phoneNumber: string) => {
 
 export default function SignUpPage() {
 
+      const [showPopup, setShowPopup] = useState(false);    
+
           return (
-                    <div className="flex flex-col min-h-screen">
-                              <header className="flex flex-row justify-between items-center p-4">
-                                        <Link href="/">
-                                                  <Button variant="ghost" className="text-xl md:text-[24px] mb-1 font-bold text-[#191c1f]">StellarPay</Button>
-                                        </Link>
-                              </header>
+            <div className="flex flex-col min-h-screen">
+            <header className="flex flex-row justify-between items-center p-2">
+              <Link href="/">
+                <Button variant="ghost" className="text-xl md:text-[24px] font-bold text-[#191c1f]">StellarPay</Button>
+              </Link>
+            </header>
+      
+      
+      
+      
+      
+            <main className="flex flex-col justify-center items-center mb-[50px]">
+      
+            <Image src="/images/stellarbanking_logo.png" alt="StellarPay" width={100} height={100} className="mx-auto mb-[20px]" />
+              <div className="flex flex-col md:flex-row justify-center items-center md:translate-x-[-24px]">
+                <div className="flex flex-col max-w-lg mr-[50px]">
+                  <h1 className="text-4xl font-semibold mb-4">Welcome</h1>
+                  <div className="max-w-md">
+                  <p className="text-md text-gray-700 mb-[40px]">Enter your phone number</p>
+                  </div>
+                  <div className="flex flex-row">
+                    <input type="text" placeholder="+1" className="mb-[20px] w-[60px] lg:h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px] mr-[20px]" onChange={(e) => { }} maxLength={4} />
+                    <input type="text" placeholder="Mobile number" className="w-[340px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px]" onChange={(e) => { }} maxLength={10} />
+                  </div>
+                  <a onClick={() => setShowPopup(true)}>
+                    <button className="bg-[#000000] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px] mt-[5px]">Continue</button>
+                    </a>
+                  <p className="text-center text-md text-[#191c1f] mt-[20px] mr-[20px]">OR</p>
+                  <div className="flex flex-row justify-between items-center border-gray mr-[20px]">
+                    <a onClick={() => setShowPopup(true)}>
+                      <button className="mt-[20px] w-[200px] h-[60px] rounded-[10px] border 
+                    border-gray-300 p-[10px] text-[18px] mr-[10px]">
+                      <div className="flex flex-row items-center">
+                        <Image src="/images/GoogleLogo.png" alt="Google" width={20} height={20} />
+                        <p className="text-[16px] ml-[6px]">Sign up with Google</p>
+                      </div>
+                    </button>
+                    </a>
+                    <a onClick={() => setShowPopup(true)}>
+                      <button className="mt-[20px] w-[200px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[18px] mr-[10px]">
+                        <div className="flex flex-row items-center">
+                          <Image src="/images/AppleLogo.png" alt="Apple" width={20} height={20} />
+                          <p className="text-[16px] ml-[10px]">Sign up with Apple</p>
+                        </div>
+                      </button>
+                    </a>
+                  </div>
+                  <p className="mt-[20px] text-center mr-[20px] text-md">Already have an account? </p>
+                  <Link href="/signin">
+                    <button className="mt-[20px] bg-[#000000] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px]">Log in</button>
+                  </Link>
+                </div>
+      
+                <div className="hidden md:block flex flex-col justify-center items-center ml-[100px]">
+                  <Image src="/images/qr-code.png" alt="QR Code" width={200} height={200} className="mb-[20px] border-gray-700 border-1 rounded-2xl shadow-xl ml-[25px]" />
+                  <p className="text-center text-lg font-bold text-[#191c1f] mt-[20px]">Sign up with QR code</p>
+                  <p className="text-center text-md text-gray-700 mt-[20px] w-[250px]">Scan this code with your phone camera to sign up instantly</p>
+                </div>
+      
+              </div>
+            </main>
+      
+      
+            <div className="fixed flex flex-row left-[60px] bottom-[30px]">
+              <p className="mr-[30px] text-sm md:text-md">English (United States)</p>
+              <Link href="/privacy-policy" className="block text-[#000000] underline hover:no-underline text-sm md:text-md">
+                Privacy policy
+              </Link>
+            </div>
 
-
-
-
-
-                              <main className="flex flex-col justify-center items-center p-8">
-
-
-                                        <div className="flex flex-row justify-center items-center">
-                                                  <div className="flex flex-col">
-                                                            <Image src="/images/stellarbanking_logo.png" alt="StellarPay" width={100} height={100} className="mx-auto mb-[20px]" />
-                                                            <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-                                                            <p className="text-md text-[#191c1f] mb-[20px]">Enter your phone number</p>
-                                                            <div className="flex flex-row">
-                                                                      <input type="text" placeholder="+1" className="mb-[20px] w-[60px] lg:h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px] mr-[10px]" onChange={(e) => { }} maxLength={4} />
-                                                                      <input type="text" placeholder="Mobile number" className="w-[340px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[20px]" onChange={(e) => { }} maxLength={10} />
-                                                            </div>
-                                                            <Link href="/api/auth/signin">
-                                                                      <button className="bg-[#0065ff] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px]">Continue</button>
-                                                            </Link>
-                                                            <p className="text-center text-md text-[#191c1f] mt-[20px]">OR</p>
-                                                            <div className="flex flex-row justify-center items-center">
-                                                                      <Link href="/api/auth/signin">
-                                                                            <button className="mt-[20px] w-[200px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[18px] mr-[10px]">
-                                                                                <div className="flex flex-row items-center">
-                                                                                      <Image src="/images/GoogleLogo.png" alt="Google" width={20} height={20} />
-                                                                                      <p className="text-[16px] ml-[10px]">Sign up with Google</p>
-                                                                                </div>
-                                                                            </button>
-                                                                      </Link>
-                                                                      <Link href="/api/auth/signin">
-                                                                            <button className="mt-[20px] w-[200px] h-[60px] rounded-[10px] border border-gray-300 p-[10px] text-[18px] mr-[10px]">
-                                                                                <div className="flex flex-row items-center">
-                                                                                      <Image src="/images/AppleLogo.png" alt="Apple" width={20} height={20} />
-                                                                                      <p className="text-[16px] ml-[10px]">Sign up with Apple</p>
-                                                                                </div>
-                                                                            </button>
-                                                                      </Link>
-                                                            </div>
-                                                            <p className="mt-[20px] text-md">Already have an account? </p>
-                                                            <Link href="/signin">
-                                                                      <button className="mt-[20px] bg-[#0065ff] text-white px-4 py-2 rounded-[10px] border border-gray-300 h-[60px] w-[420px] text-[20px]">Log in</button>
-                                                            </Link>
-                                                  </div>
-
-                                                  <div className="flex flex-col justify-center items-center ml-[100px]">
-                                                            <Image src="/images/qr-code.png" alt="QR Code" width={200} height={200} className="mb-[20px]" />
-                                                            <p className="text-center text-lg font-bold text-[#191c1f]">Sign up with QR code</p>
-                                                            <p className="text-center text-md text-[#191c1f] mt-[20px] w-[300px]">Scan this code with your phone camera to Sign up instantly</p>
-                                                  </div>
-
-                                        </div>
-                              </main>
-
-
-                              <div className="absolute flex flex-row left-[40px] bottom-[10px]">
-                                    <p className="mr-[60px] text-sm md:text-md">English (United States)</p>
-                                    <Link href="/privacy-policy" className="block text-[#000000] underline hover:no-underline text-sm md:text-md">
-                                    Privacy policy
-                                    </Link>
-                              </div>
-
-                    </div>
+            {showPopup && <GetTheApp onClose={() => setShowPopup(false)} onSubmit={() => {}} />}
+      
+          </div>
           );
 } 
