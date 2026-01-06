62ca6512bfcb47a3ceb27cd0d8a11b2ba481e48c
rohiths2 <rohiths2@illinois.edu>
2026-01-06 16:01:34 -0600

    trial

--- app/(public)/linq/contact/save_contact_info.ts
+++ app/(public)/linq/contact/save_contact_info.ts
@@ -60,7 +60,9 @@ export const saveContactInfo = async (
     country: country || '',
   };
 
-  window.location.href = "/linq/thankyou";
+  // window.location.href = "/linq/thankyou";
+
+  console.log(DASHBOARD_SYNC_URL)
 
   const headers: HeadersInit = {
     "Content-Type": "application/json",
@@ -76,13 +78,14 @@ export const saveContactInfo = async (
     keepalive: true,
   })
     .then((response) => {
+      console.log(response)
       if (!response.ok) {
         throw new Error(`API call failed with status ${response.status}`);
       }
     })
     .catch((error) => {
       console.error('Error creating signup:', error);
-      window.location.href = "/linq/contact";
+      // window.location.href = "/linq/contact";
     });
 };
 
