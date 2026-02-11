aa75bd5d32a6df867984482c7baf51a83a07ad70
rohiths2 <rohiths2@illinois.edu>
2026-02-10 20:23:57 -0600

    android link

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function getUrl() {
           const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
           if (/android/i.test(userAgent)) {
-            window.location.href = 'https://play.google.com/store/apps/dev?id=8934712149181103165';
+            window.location.href = 'https://play.google.com/store/apps/details?id=com.stellar.mobile';
             return;
           }
           if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
@@ -14,7 +14,7 @@ function getUrl() {
                     window.location.href = 'https://apps.apple.com/app/stellarpay/id6450455712';
                     return;
           }
-          window.location.href = 'https://play.google.com/store/apps/dev?id=8934712149181103165';
+          window.location.href = 'https://play.google.com/store/apps/details?id=com.stellar.mobile';
 }
 export default function Download() {
           useEffect(() => {
