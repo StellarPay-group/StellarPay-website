9911a31483088da1144c6bc2c666402a8f7be6ad
rohiths2 <rohiths2@illinois.edu>
2026-03-30 12:52:42 -0500

    1

--- backend_urls.ts
+++ backend_urls.ts
@@ -1,4 +1,4 @@
-export const SERVER = 'https://api.stellarpay.app'
+export const SERVER = 'https://staging-api.stellarpay.app'
 export const DASHBOARD_SYNC_URL =
   process.env.NEXT_PUBLIC_DASHBOARD_SYNC_URL ||
   process.env.DASHBOARD_SYNC_URL ||
