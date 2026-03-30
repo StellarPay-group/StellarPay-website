a4b233e5eb954122afed61b6e5ac43f7e04aa7c8
rohiths2 <rohiths2@illinois.edu>
2026-03-30 12:50:23 -0500

    updated API URLs

--- backend_urls.ts
+++ backend_urls.ts
@@ -1,4 +1,4 @@
-export const SERVER = 'https://6muo0y8506.execute-api.us-east-1.amazonaws.com/'
+export const SERVER = 'https://api.stellarpay.app'
 export const DASHBOARD_SYNC_URL =
   process.env.NEXT_PUBLIC_DASHBOARD_SYNC_URL ||
   process.env.DASHBOARD_SYNC_URL ||
